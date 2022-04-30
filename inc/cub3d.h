0d5ec2770374aae44e11006184ddab982d3ff4d9
YvanoffP <petruzzi.yvan@gmail.com>
2022-04-30 22:41:31 +0200

    y does not work, I asked Bima to redo the map gathering to comply with some things I needed <3

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -76,6 +76,7 @@ int		game_infos(t_game *game, t_parse *control);
 //PARSING - tools.c
 int		is_w_space(char c);
 void	skip_white_space(char *str, int *i);
+void	vertical_skip_white_space(char **str, int *i, int x, int max_i);
 
 //PARSING - map_checker.c
 int     check_map(char **map);
