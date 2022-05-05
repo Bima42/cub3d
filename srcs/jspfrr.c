856930fdd1b678b838eee4f8ef1a11fc78b19ee2
YvanoffP <petruzzi.yvan@gmail.com>
2022-05-05 22:03:59 +0200

    idk how to name this file D:

---
+++ srcs/jspfrr.c
@@ -0,0 +1,7 @@
+#include "../../inc/cub3d.h"
+
+void	texture_loader(t_game *game)
+{
+	// IT'S FROM SO LONG I DID NOT CHANGE A SINGLE THING
+	load_one_texture(*game, &(*game)->sprite.player_1, "./img/player_1.xpm");
+}
