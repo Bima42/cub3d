83b1901a8d572dadfc53beb550a11164b6bedaf1
YvanoffP <petruzzi.yvan@gmail.com>
2022-05-07 14:53:12 +0200

    small changes

--- srcs/jspfrr.c
+++ srcs/jspfrr.c
@@ -15,6 +15,7 @@ void	create_window(t_game *game)
 {
 	game->win->x = 1920;
 	game->win->y = 1024;
+	exit(0);
 	game->win->mlx_win = mlx_new_window(game->mlx,
 			game->win->x, game->win->y, "cub3d");
 	game->win->img = mlx_new_image(game->mlx,
